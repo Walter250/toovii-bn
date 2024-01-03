e46f5fa363ad426b548e29da4f141c9cd39d0d13
aristidei <a.isingizwe@alustudent.com>
2024-01-03 17:25:24 +0200

    auth guards

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,6 +7,7 @@ import {
   Patch,
   Post,
   Session,
+  UseGuards,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { NewUserDto } from './dtos/newUser.dto';
@@ -17,6 +18,7 @@ import { AuthService } from './auth.service';
 import { UserLoginDto } from './dtos/user-login.dto';
 import { User } from './user.entity';
 import { currentUser } from './decorators/current-user.decorator';
+import { AuthGuard } from 'src/guards/auth.guard';
 
 @Controller('auth')
 @Serialize(UserDto)
@@ -32,6 +34,7 @@ export class UsersController {
   // }
 
   @Get('/who')
+  @UseGuards(AuthGuard)
   who(@currentUser() user: User) {
     return user;
   }
@@ -61,16 +64,19 @@ export class UsersController {
   }
 
   @Get('/:id')
+  @UseGuards(AuthGuard)
   findUser(@Param('id') id: string) {
     return this.userService.findOne(parseInt(id));
   }
 
   @Delete('/:id')
+  @UseGuards(AuthGuard)
   removeUser(@Param('id') id: string) {
     return this.userService.remove(parseInt(id));
   }
 
   @Patch('/:id')
+  @UseGuards(AuthGuard)
   updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
     return this.userService.update(parseInt(id), body);
   }
