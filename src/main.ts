c7250b099086fbc32d3a38050c753e74d1da5678
aristidei <a.isingizwe@alustudent.com>
2024-01-03 17:40:50 +0200

    swagger init

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 const cookieSession = require('cookie-session');
 
 async function bootstrap() {
@@ -16,6 +17,15 @@ async function bootstrap() {
       transform: false,
     }),
   );
+  const config = new DocumentBuilder()
+    .setTitle('TooVii Swagger')
+    .setDescription('Full Documentation  of Toovii backend using NestJs')
+    .setVersion('1.0')
+    .addTag('Routes')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
